224628d611c262de29dcf814b37aabbf02363b79
uditha <udtihac@gmail.com>
2023-11-23 22:01:16 +0100

    first commit

---
+++ src/main/java/com/udithac/taskmanagerbackend/TaskManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.udithac.taskmanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udithac/taskmanagerbackend/TaskManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udithac.taskmanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
